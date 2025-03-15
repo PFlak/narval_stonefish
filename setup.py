8a68ec573b8fc90329beec14c1e5e09914e973a5
PFlak <piotr.andrzej.flak@gmail.com>
2025-03-15 13:20:23 +0100

    stonefish bluerov2

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = "narval_stonefish"
+
+data_files_list = []
+for f in glob(os.path.join("data", "**", "*"), recursive=True):
+    if os.path.isfile(f):  # Only copy files, not directories
+        relative_path = os.path.relpath(f, "data")
+        target_dir = os.path.join("share", package_name, "data")
+        if os.path.dirname(relative_path):  # If inside a subdirectory, preserve it
+            target_dir = os.path.join(target_dir, os.path.dirname(relative_path))
+        data_files_list.append((target_dir, [f]))
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py*'))),
+        (os.path.join('share', package_name, 'scenarios'), glob(os.path.join('scenarios','*.scn*'))),
+        *data_files_list
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Piotr Flak",
+    maintainer_email="piotrflak@student.agh.edu.pl",
+    description="Narval stonefish simulation",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
